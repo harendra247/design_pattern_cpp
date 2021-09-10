24d4c1e51245457b033cd749353c5cf5bd2ca6a0
Harendra <16508543+harendra247@users.noreply.github.com>
2021-09-10 18:41:44 +0530

    Update singleton.cpp

--- singleton.cpp
+++ singleton.cpp
@@ -1,3 +1,11 @@
+/*
+ * Example of `Singleton' design pattern (Creational Pattern).
+ * Singleton pattern is defined as ensuring that only a 
+ * single instance of a class exists and a global point 
+ * of access to it exists.
+ */
+
+
 #include <iostream>
 #include <thread> 
 #include <mutex>
