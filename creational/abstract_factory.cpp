e4c8b21526333b03da1ed14558548f38e4d2620a
Harendra <16508543+harendra247@users.noreply.github.com>
2023-10-10 10:36:12 +0530

    Update abstract_factory.cpp

--- creational/abstract_factory.cpp
+++ creational/abstract_factory.cpp
@@ -11,160 +11,301 @@
  * 5. Client
  */
 
-#include <iostream>
-#include <string>
+#include<iostream>
+using namespace std;
 
-class Window
-{
-    protected:
-        int width;
-        int height;
-        std::string toolkit;
-        std::string type;
-
-        Window(std::string usedToolkit, std::string windowType)
-            : toolkit(usedToolkit), type(windowType)
-        {}
-
-    public:
-        std::string getToolkit()
-        {
-            return toolkit;
-        }
-
-        std::string getType()
-        {
-            return type;
-        }
+class Vehicle{
+public:
+    virtual void book(int distance) = 0;
+    virtual void setVehicleType() = 0;
+    virtual void setBaseCost() = 0;
+    virtual void setVehicleChargesPerUnitDistance() = 0;
+    virtual int calculateCostOfBooking(int distance) = 0;
 };
 
-class GtkToolboxWindow : public Window
-{
-    public:
-        GtkToolboxWindow()
-            : Window("Gtk", "ToolboxWindow")
-        {}
-};
+class Car: public Vehicle {
+public:
+    string carType;
+    int baseCost;
+    int chargesPerUnitDistance;
 
-class GtkLayersWindow : public Window
-{
-    public:
-        GtkLayersWindow()
-            : Window("Gtk", "LayersWindow")
-        {}
-};
-
-class GtkMainWindow : public Window
-{ 
-    public:
-        GtkMainWindow()
-            : Window("Gtk", "MainWindow")
-        {}
-};
-
-
-class QtToolboxWindow : public Window
-{
-    public:
-        QtToolboxWindow()
-            : Window("Qt", "ToolboxWindow")
-        {}
-};
-
-class QtLayersWindow : public Window
-{
-    public:
-        QtLayersWindow()
-            : Window("Qt", "LayersWindow")
-        {}
-};
-
-class QtMainWindow : public Window
-{
-    public:
-        QtMainWindow()
-            : Window("Qt", "MainWindow")
-        {}
-};
-
-
-/* This is the abstract factory. */
-class UIFactory
-{
-    public:
-        virtual Window* getToolboxWindow() = 0;
-        virtual Window* getLayersWindow() = 0;
-        virtual Window* getMainWindow() = 0;
-
-};
-
-/* Factory for Gtk toolkit */
-class GtkUIFactory : public UIFactory
-{
-    public:
-        Window* getToolboxWindow()
-        {
-            return new GtkToolboxWindow();
-        }
-
-        Window* getLayersWindow()
-        {
-            return new GtkLayersWindow();
-        }
-
-        Window* getMainWindow()
-        {
-            return new GtkMainWindow();
-        }
-};
-
-/* Factory for Qt toolkit */
-class QtUIFactory : public UIFactory
-{
-    public:
-        Window* getToolboxWindow()
-        {
-            return new QtToolboxWindow();
-        }
-
-        Window* getLayersWindow()
-        {
-            return new QtLayersWindow();
-        }
-
-        Window* getMainWindow()
-        {
-            return new QtMainWindow();
-        }
-};
-
-int main()
-{
-    UIFactory* ui = 0;
-
-    /* Check what environment is running
-       and create appropriate factory. */
-    if (/* Gtk == */ true)
-    {
-        ui = new GtkUIFactory();
-    }
-    else
-    {
-        ui = new QtUIFactory();
+    void book(int distance) override {
+        setVehicleType();
+        setBaseCost();
+        setVehicleChargesPerUnitDistance();
+        int cost = calculateCostOfBooking(distance);
+        cout<<"You have booked a "<<carType<<" Car for a distance of "<<distance<<" kms at a total cost of "<<cost<<". "<<endl;
     }
 
-    /* Use the factory to build interface. */
-    Window* toolbox = ui->getToolboxWindow();
-    Window* layers  = ui->getLayersWindow();
-    Window* main    = ui->getMainWindow();
+    int calculateCostOfBooking(int distance) override {
+        int serviceCharge = 3;
+        return baseCost + chargesPerUnitDistance * distance + serviceCharge;
+    }
 
-    /* See what have we recieved. */
-    std::cout << toolbox->getToolkit() << ":"
-              << toolbox->getType() << std::endl;
+};
 
-    std::cout << layers->getToolkit() << ":"
-              << layers->getType() << std::endl;
+class MicroCar: public Car {
+public:
+    MicroCar(){}
 
-    std::cout << main->getToolkit() << ":"
-              << main->getType() << std::endl;
+    void setVehicleType() override {
+        carType = "Micro";
+    }
+
+    void setBaseCost() override {
+        baseCost = 50;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 10;
+    }
+};
+
+class MiniCar: public Car {
+public:
+    MiniCar(){}
+
+    void setVehicleType() override {
+        carType = "Mini";
+    }
+
+    void setBaseCost() override {
+        baseCost = 100;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 20;
+    }
+};
+
+class MegaCar: public Car {
+public:
+    MegaCar(){}
+
+    void setVehicleType() override {
+        carType = "Mega";
+    }
+
+    void setBaseCost() override {
+        baseCost = 150;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 30;
+    }
+};
+
+
+class Auto: public Vehicle {
+public:
+    string shareType;
+    int baseCost;
+    int chargesPerUnitDistance;
+
+    void book(int distance) override {
+        setVehicleType();
+        setBaseCost();
+        setVehicleChargesPerUnitDistance();
+        int cost = calculateCostOfBooking(distance);
+        cout<<"You have booked a "<<shareType<<" Autorickshaw for a distance of "<<distance<<" kms at a total cost of "<<cost<<". "<<endl;
+    }
+
+    int calculateCostOfBooking(int distance) override {
+        int serviceCharge = 1;
+        return baseCost + chargesPerUnitDistance * distance + serviceCharge;
+    }
+};
+
+class PersonalAuto: public Auto {
+public:
+    PersonalAuto(){}
+
+    void setVehicleType() override {
+        shareType = "Personal";
+    }
+
+    void setBaseCost() override {
+        baseCost = 10;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 15;
+    }
+};
+
+class SharedAuto: public Auto {
+public:
+    SharedAuto(){}
+
+    void setVehicleType() override {
+        shareType = "Shared";
+    }
+
+    void setBaseCost() override {
+        baseCost = 0;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 10;
+    }
+};
+
+
+class Bike: public Vehicle {
+public:
+    string bikeType;
+    int baseCost;
+    int chargesPerUnitDistance;
+
+    void book(int distance) override {
+        setVehicleType();
+        setBaseCost();
+        setVehicleChargesPerUnitDistance();
+        int cost = calculateCostOfBooking(distance);
+        cout<<"You have booked a "<<bikeType<<" Bike for a distance of "<<distance<<" kms at a total cost of "<<cost<<". "<<endl;
+    }
+
+    int calculateCostOfBooking(int distance) override {
+        return baseCost + chargesPerUnitDistance * distance;
+    }
+};
+
+class SportsBike: public Bike {
+public:
+    SportsBike(){}
+
+    void setVehicleType() override {
+        bikeType = "Sports";
+    }
+
+    void setBaseCost() override {
+        baseCost = 40;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 15;
+    }
+};
+
+class NormalBike: public Bike {
+public:
+    NormalBike(){}
+
+    void setVehicleType() override {
+        bikeType = "Normal";
+    }
+
+    void setBaseCost() override {
+        baseCost = 20;
+    }
+
+    void setVehicleChargesPerUnitDistance() override {
+        chargesPerUnitDistance = 5;
+    }
+};
+
+class AbstractVehicleFactory {
+public:
+    virtual Vehicle*getVehicle(string type) = 0;
+};
+
+class CarFactory: public AbstractVehicleFactory {
+public:
+    CarFactory(){}
+
+    Vehicle*getVehicle(string type) override {
+        if(type.compare("Micro") == 0){
+            return new MicroCar();
+        }
+        else if(type.compare("Mini") == 0){
+            return new MiniCar();
+        }
+        else if(type.compare("Mega") == 0){
+            return new MegaCar();
+        }
+        else{
+            return new MiniCar();
+        }
+    }	
+};
+
+class AutoFactory: public AbstractVehicleFactory {
+public:
+    AutoFactory(){}
+
+    Vehicle*getVehicle(string type) override {
+        if(type.compare("Personal") == 0){
+            return new PersonalAuto();
+        }
+        else if(type.compare("Shared") == 0){
+            return new SharedAuto();
+        }
+        else{
+            return new PersonalAuto();
+        }
+    }	
+};
+
+class BikeFactory: public AbstractVehicleFactory {
+public:
+    BikeFactory(){}
+
+    Vehicle*getVehicle(string type) override {
+        if(type.compare("Sports") == 0){
+            return new SportsBike();
+        }
+        else if(type.compare("Normal") == 0){
+            return new NormalBike();
+        }
+        else{
+            return new NormalBike();
+        }
+    }	
+};
+
+class FactoryProvider {
+public:
+    static AbstractVehicleFactory*getVehicleFactory(string factoryType){
+        if(factoryType.compare("Car") == 0){
+            return new CarFactory();
+        }
+        else if(factoryType.compare("Auto") == 0){
+            return new AutoFactory();
+        }
+        else if(factoryType.compare("Bike") == 0){
+            return new BikeFactory();
+        }
+        else{
+            return new CarFactory();
+        }
+    }
+};
+
+int main(){
+    int distance = 10;
+
+    /*
+    * Book a Mini Car for a distance of 10 kms
+    */
+    AbstractVehicleFactory*carFactory = FactoryProvider::getVehicleFactory("Car");
+    Vehicle*miniCar = carFactory->getVehicle("Mini");
+    miniCar->book(distance);
+
+
+    /*
+    * Book a Personal Auto for a distance of 10 kms
+    */
+    AbstractVehicleFactory*autoFactory = FactoryProvider::getVehicleFactory("Auto");
+    Vehicle*personalAuto = autoFactory->getVehicle("Personal");
+    personalAuto->book(distance);
+
+
+    /*
+    * Book a Sports Bike for a distance of 10 kms
+    */
+    AbstractVehicleFactory*bikeFactory = FactoryProvider::getVehicleFactory("Bike");
+    Vehicle*sportsBike = bikeFactory->getVehicle("Sports");
+    sportsBike->book(distance);
+
+    return 0;
 }
